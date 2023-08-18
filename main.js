8d439361f92b6a2f0162ccf4f7dac4629d2c2b7f
AndreyAnklav <142445234+AndreyAnklav@users.noreply.github.com>
2023-08-18 23:27:52 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,32 @@
-console.log("Hello java");
-console.log(123435);
-console.log('Hello' + ' java');
+// console.log("Hello java");
+// console.log(123435);
+// console.log('Hello' + ' java');
+// console.log(12);
+// console.log("декабрь");
+// console.log('Добро ' + 'пожаловать ' + 'на курс, '+ 'Андрей');
+// let a = "Добро пожаловать на курс, Андрей"
+// console.log(a);
+// alert('18 августа 2023г.'); 
+document.getElementById('one').innerHTML ='Hello World';
+console.log(5/0);
+console.log('free' - 123);
+console.log(23429883546987628634823498n);
+let a = "Дом"
+let b = 'Воли'
+let c = `Сила ${b}`
+console.log(a);
+console.log(b);
+console.log(c);
+let future = true
+let nofuture = false
+console.log(future);
+console.log(nofuture);
+let nuller = null
+console.log(nuller);
+let past = undefined;
+console.log(past);
+console.log(typeof 5);
+console.log(typeof 34664095698459086n);
+console.log(typeof false);
+console.log(typeof 'бегущаяя строка');
+console.log(typeof null);
