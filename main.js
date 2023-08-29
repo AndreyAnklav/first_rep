3aed6dec2ad947f18ade138fc0348a0c256b9e31
Андрей Лозовой <9537312317and@gmail.com>
2023-08-29 21:34:22 +0300

    added a sixth task

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // console.log("Hello java");
 // console.log(123435);
 // console.log('Hello' + ' java');
@@ -45,7 +44,7 @@
 // document.querySelector('p').innerHTML = 'Однако, при нескольких записях подряд, информация будет выводиться сверху вниз с большой скоростью и на странице останется только последняя запись'
 // let inputIN = document.querySelector(".text")
 // let btn = document.querySelector('.check')
-let div = document.querySelector('.out')
+// let div = document.querySelector('.out')
 
 // btn.onclick = function(){
 //     let e = inputIN.value
@@ -55,7 +54,7 @@ let div = document.querySelector('.out')
 //     console.log(inputIN.value);
 //     console.log(typeof e);
 // }
-        //   3 варианта с заданием if else
+	//   3 варианта с заданием if else
 
 // let x = 109;
 // if (x < 100){
@@ -123,88 +122,70 @@ let div = document.querySelector('.out')
 
 // Задание#5-3
 
-for (let i = 2500; i <= 3000; i++){
-        if (i % 2 == 0) continue;
-        console.log(i);
-}
-=======
-// console.log("Hello java");
-// console.log(123435);
-// console.log('Hello' + ' java');
-// console.log(12);
-// console.log("декабрь");
-// console.log('Добро ' + 'пожаловать ' + 'на курс, '+ 'Андрей');
-// let a = "Добро пожаловать на курс, Андрей"
-// console.log(a);
-// alert('18 августа 2023г.'); 
-// document.getElementById('one').innerHTML ='Hello World';
-// console.log(5/0);
-// console.log('free' - 123);
-// console.log(23429883546987628634823498n);
-// let a = "Дом"
-// let b = 'Воли'
-// let c = `Сила ${b}`
-// console.log(a);
-// console.log(b);
-// console.log(c);
-// let future = true
-// let nofuture = false
-// console.log(future);
-// console.log(nofuture);
-// let nuller = null
-// console.log(nuller);
-// let past = undefined;
-// console.log(past);
-// console.log(typeof 5);
-// console.log(typeof 34664095698459086n);
-// console.log(typeof false);
-// console.log(typeof 'бегущаяя строка');
-// console.log(typeof null);
-
-// document.getElementById('one').innerHTML ="<h2>заголовок1</h2>"
-// document.getElementById('one').innerHTML = 4567
-// document.getElementById('one').innerHTML ="<h2>Текст</h2>"
-
-// let jojo = 'jojo'
-// jojo = 'Андрей'
-// console.log(jojo);
-
-// document.querySelector('#one').innerHTML = '<h2>Забава</h2>'
-
-// document.querySelector('p').innerHTML = 'Однако, при нескольких записях подряд, информация будет выводиться сверху вниз с большой скоростью и на странице останется только последняя запись'
-// let inputIN = document.querySelector(".text")
-// let btn = document.querySelector('.check')
-let div = document.querySelector('.out')
-
-// btn.onclick = function(){
-//     let e = inputIN.value
-//     div.innerHTML += `${e}<br>`
-//     console.log('Да');
-//     console.log(e);
-//     console.log(inputIN.value);
-//     console.log(typeof e);
-// }
-        //   3 варианта с заданием if else
-
-// let x = 109;
-// if (x < 100){
-//     console.log(x);
-// } else {
-//     alert(x);
+// for (let i = 2500; i <= 3000; i++){
+//         if (i % 2 == 0) continue;
+//         console.log(i);
 // }
 
-// let x = 42;
-// if (x < 100){
-//     console.log(x);
-//     } else {
-//         document.getElementById("one").innerHTML = x;
-//     }
+// Задание 6-1
 
-let x = prompt('Куда вывести число?');
-if (x > 100){
-    alert(x)
-} else {
-    console.log(x);
-}
+// let a = 2;
+// let b = prompt('Введите число 2');
+// switch (+b){
+// 	case a:
+// 		alert('Да');
+// 		break;
+// 	case 1:
+// 	case 3:
+// 	case 4:
+// 	case 5:
+// 	case 6:
+// 	case 7:
+// 	case 8:
+// 	case 9:
+// 		alert('Нет');
+// 		break;
+// 	default:
+// 		alert('Не сработало');
+// }
 
->>>>>>> 079398fc4407dc4185746408af56feee9a615ffa
+// Задание 6-2
+
+// let month = prompt('Введите число от 1 до 12, чтобы узнать время года');
+// switch(+month){
+// 	case 1:
+// 	case 2:
+// 	case 3:
+// 		alert('Зима');
+// 		break;
+// 	case 4:
+// 	case 5:
+// 	case 6:
+// 		alert('Весна');
+// 		break;
+// 	case 7:
+// 	case 8:
+// 	case 9:
+// 		alert('Лето');
+// 		break;
+// 	case 10:
+// 	case 11:
+// 	case 12:
+// 		alert('Осень');
+// 		break;
+// 	default:
+// 		alert('Такого сезона нет');
+
+// }
+
+// Задание 6-3
+
+let one = null;
+let two = 'Привет, мир!';
+let three = 345678;
+console.log(one ?? two ?? three);
+
+
+
+
+
