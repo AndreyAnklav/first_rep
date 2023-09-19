f6721e2b5e8a975f2b84f0916bd6b902d42b66e2
Андрей Лозовой <9537312317and@gmail.com>
2023-09-19 22:54:46 +0300

    Homework14

--- main.js
+++ main.js
@@ -467,20 +467,56 @@
 
 // console.log(3);
 
-let xhttp = new XMLHttpRequest()
-xhttp.onreadystatechange = function(){
-	if(this.readyState == 4 && this.status == 200){
-		myfunc(this.responseText)
-	}
-}
+// let xhttp = new XMLHttpRequest()
+// xhttp.onreadystatechange = function(){
+// 	if(this.readyState == 4 && this.status == 200){
+// 		myfunc(this.responseText)
+// 	}
+// }
 
-xhttp.open('GET', "https://automarine25.ru/", true)
-xhttp.send()
+// xhttp.open('GET', "https://automarine25.ru/", true)
+// xhttp.send()
 
-function myfunc(data){
-	console.log(data);
-}
+// function myfunc(data){
+// 	console.log(data);
+// }
 
+// Задание 14
+
+// xhttp.open("GET", "https://automarine25.ru/&quot")
+// xhttp.send()
+// function myfunc(data){
+//   console.log(data);
+// }
+// fetch('http://127.0.0.1:5500/index.html')
+// .then(data => data.text()
+// ).then(data => {
+// console.log(data);
+// })
+
+let a = new Promise((resolve, reject) =>{
+	fetch('http://127.0.0.1:5500/index.html')
+	.then(data =>{
+		resolve(data.text())
+		})
+	})
+let b = new Promise((resolve, reject) =>{
+	fetch('http://127.0.0.1:5500/index.html')
+	.then(data =>{
+		resolve(data.text())
+		})
+	})
+
+	// a.then(data => {
+	// console.log(data);
+	// })
+	// b.then(data => {
+	// console.log(data);
+	// })
+
+	Promise.all([a,b]).then(data => {
+		console.log(data);
+	})
 
 
 
